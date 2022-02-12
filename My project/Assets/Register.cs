63a015c5e926d5ce80345e39a33d52df424bf34e
Gu <76677804+sleepingvoice@users.noreply.github.com>
2022-02-12 22:19:59 +0900

    phpmyadmin을 통한 회원가입

---
+++ My project/Assets/Register.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Register : MonoBehaviour
+{
+	public Text Id;
+	public Text PassWord;
+	public Button signup;
+
+	private string User_code;
+
+	private void Awake()
+    {
+		signup.onClick.AddListener(() => RegisterBtk());
+	}
+
+	public void RegisterBtk()
+    {
+		if(Id.text != null && PassWord.text != null)
+        {
+			StartCoroutine(Register_ID());
+		}
+    }
+
+	IEnumerator Register_ID()
+    {
+		UnityWebRequest CheckCode = UnityWebRequest.Get("http://localhost/Test/RegisterCheck.php");
+
+		yield return CheckCode.SendWebRequest();
+
+		if (CheckCode.error != null)
+		{
+			Debug.Log("번호 받아오기 실패");
+			yield return null;
+		}
+		else
+		{
+			string CodeString = CheckCode.downloadHandler.text;
+			int code = (int.Parse(CodeString));
+			code++;
+			User_code = code.ToString("000000");
+		}
+
+		WWWForm form = new WWWForm();
+		form.AddField("ID_Post", Id.text);
+		form.AddField("PassWord_Post", PassWord.text);
+		form.AddField("User_code_Post", User_code);
+		UnityWebRequest www = UnityWebRequest.Post("http://localhost/Test/register.php", form);
+
+		yield return www.SendWebRequest();
+
+		if (www.error == null)
+			Debug.Log("성공했습니다.");
+		else
+			Debug.Log("실패했습니다.");
+	}
+}
---
+++ My project/Assets/Script/Test_Server.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Login : MonoBehaviour
+{
+
+	public string inputUserName;
+	public string inputPassword;
+
+	string LoginURL = "http:///login.php";
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+			StartCoroutine(LoginToDB(inputUserName, inputPassword));
+	}
+
+	IEnumerator LoginToDB(string username, string password)
+	{
+		WWWForm form = new WWWForm();
+		form.AddField("usernamePost", username);
+		form.AddField("passwordPost", password);
+
+		WWW www = new WWW(LoginURL, form);
+
+		yield return www;
+		Debug.Log(www.text);
+
+	}
+}
